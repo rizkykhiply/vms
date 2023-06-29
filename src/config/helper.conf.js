143ba6dc54ca98074f54415fe290b9c06283043e
rendsyah <rndyfrdynsyh@gmail.com>
2023-06-29 11:50:57 +0700

    update validateImage

--- src/config/helper.conf.js
+++ src/config/helper.conf.js
@@ -37,8 +37,8 @@ module.exports.validateRandomChar = (request, type) => {
 // Define Validate Image
 module.exports.validateImage = (params) => {
     const getPath = params.path;
-    const getImageScan = params.imageScan;
-    const getImageCam = params.imageCam;
+    const getImageScan = params?.imageScan;
+    const getImageCam = params?.imageCam;
 
     if (!fs.existsSync(getPath)) {
         fs.mkdirSync(getPath, { recursive: true });
@@ -49,6 +49,10 @@ module.exports.validateImage = (params) => {
 
     for (let i = 0; i < getImages.length; i++) {
         const getImage = getImages[i];
+
+        if (!getImage) {
+            continue;
+        }
 
         const getCurrDate = new Date().getTime();
         const getRandChar = this.validateRandomChar(8, 'alphanumeric');
